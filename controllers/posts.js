d86e71d38628f4cdde607407afcd8be9fe2420a1
itamar reiter <itamarreiter@gmail.com>
2023-05-17 13:40:37 +0300

    added posts list sorting by friendship

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,38 @@
 const Posts = require('../models/post');
+const Users = require('../models/user');
 const InvalidDataError = require('../utils/errors/InvalidDataError');
 const NotFoundError = require('../utils/errors/NotFoundError');
 const ServerError = require('../utils/errors/ServerError');
 
+
 // get all posts
 const getPosts = (req, res, next) => {
   const userId = req.user._id;
+  let user = {};
+  Users.findOne({ _id: userId })
+    .then((userOutput) => {
+      user = userOutput;
+    })
+    .catch(next);
+
   Posts.find({})
     .then((posts) => {
+      // sorting potsts by number of comments
       const sortedPosts = posts.sort((a, b) => b.comments.length - a.comments.length);
-      const postsWithIsLiked = sortedPosts.map((post) => {
+      // sorting posts by friendship
+      const sortPostsByFriendship = sortedPosts.sort((a, b) => {
+        const aFriendship = user._doc.friends.includes(a._doc.owner);
+        const bFriendship = user._doc.friends.includes(b._doc.owner);
+        if (aFriendship && !bFriendship) {
+          return -1;
+        }
+        if (!aFriendship && bFriendship) {
+          return 1;
+        }
+        return 0;
+      });
+      // adding isLiked property to each post, based on user's likes
+      const postsWithIsLiked = sortPostsByFriendship.map((post) => {
         if (post._doc.likes.includes(userId)) {
           post._doc.isLiked = true;
         } else {
